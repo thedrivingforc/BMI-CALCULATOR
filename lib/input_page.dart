f41762b79805b6ecc106887aa244903c1a8734b1
thedrivingforce <thedrivingforc@users.noreply.github.com>
2021-01-12 22:51:31 +0530

    second screen attached

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -216,6 +217,17 @@ class _InputPageState extends State<InputPage> {
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: kBottomContainerHeight,
+            child: GestureDetector(
+              child: Text('Click Me'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultPage(),
+                  ),
+                );
+              },
+            ),
           )
         ],
       ),
